de11953cec0145331c771841b8115aa50394c7bb
g1527037 <g1527037@st.t-kougei.ac.jp>
2016-06-27 17:32:03 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,8 +1,15 @@
-for(var i = 1; i < 101; i++){
+/*for(var i = 1; i < 101; i++){
   var li=document.CreateElement('li');
   li.textContent= i;
   document.getElementById('list').appendChild(li);
-};
+};*/
+
+for(var i = 1; i < 10; i++){
+  var li = document.createElement('li');
+  li.textContent = i + '回目';
+  document.getElementById('list').appendChild(li);
+}
+
 document.getElementById('form').onsubmit = function(){
   window.alert(document.getElementById('!').word.value);
 };
