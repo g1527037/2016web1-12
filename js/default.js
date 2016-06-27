bc26885a30fdb487850a6fdf99fc512241e85082
g1527037 <g1527037@st.t-kougei.ac.jp>
2016-06-27 17:57:18 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -5,6 +5,9 @@ for(var i = 1; i < 101; i++){
   if(i%3=0){
     li.textContent = Fizz;
     }*/
+    if i % 3 == 0 then 
+   print "Fizz" 
+   
   document.getElementById('list').appendChild(li);
 }
 
