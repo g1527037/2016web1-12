6fd899034b8da9d5f3dc4e5bb9acb52fe4256325
g1527037 <g1527037@st.t-kougei.ac.jp>
2016-06-27 17:44:22 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -6,7 +6,7 @@
 
 for(var i = 1; i < 101; i++){
   var li = document.createElement('li');
-  li.textContent = i + '回目';
+  li.textContent = i % 3 = Fizz ;
   document.getElementById('list').appendChild(li);
 }
 
