e8e9c1f122b69ddabaca2f1a39916d17ea27d0e9
g1527037 <g1527037@st.t-kougei.ac.jp>
2016-06-27 17:46:29 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,12 +1,10 @@
-/*for(var i = 1; i < 101; i++){
-  var li=document.CreateElement('li');
-  li.textContent= i;
-  document.getElementById('list').appendChild(li);
-};*/
 
 for(var i = 1; i < 101; i++){
   var li = document.createElement('li');
-  li.textContent = i % 3 = Fizz ;
+  li.textContent = i ;
+  if(i%3=0){
+    return Fizz;
+    }
   document.getElementById('list').appendChild(li);
 }
 
