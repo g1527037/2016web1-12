f7b5d95c299e6dad61a6d1a0fed6bdbf812fc8ed
g1527037 <g1527037@st.t-kougei.ac.jp>
2016-06-27 17:40:25 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -4,7 +4,7 @@
   document.getElementById('list').appendChild(li);
 };*/
 
-for(var i = 1; i < 10; i++){
+for(var i = 1; i < 101; i++){
   var li = document.createElement('li');
   li.textContent = i + '回目';
   document.getElementById('list').appendChild(li);
