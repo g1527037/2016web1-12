afa79dcb85e1129802b9b92006c38366e9cad9a2
g1527037 <g1527037@st.t-kougei.ac.jp>
2016-06-27 17:21:24 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,5 +1,8 @@
-for(var i = 1;i<101;i++){
+for(var i = 1; i < 101; i++){
   var li=document.CreateElement('li');
   li.textContent= i;
   document.getElementById('list').appendChild(li);
-}
+};
+document.getElementById('form').onsubmit = function(){
+  window.alert('!');
+};
